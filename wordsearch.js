b844deae5ea1f3de73666edcb5d18f72c20d3e4f
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-19 18:20:46 -0500

    Add tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  // Check if letters or word parameters are empty
+  if (letters.length === 0 || word.length === 0) {
+    return "Error wrong input";
+  }
 
-module.exports = wordSearch
+  // Obtain horizontal arrays - provided by instructions
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+
+  // Create new empty array
+  let newArr = [];
+
+  // Loop through first array of matrix to push in correct number of arrays
+  for (let i = 0; i < letters[0].length; i++) {
+    newArr.push([]);
+  }
+
+  //  Loop through each element of the matrix and push to new arr
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      // Capture the element and push into the new array
+      newArr[j].push(letters[i][j]);
+    }
+  }
+
+  // Join elements in array
+  const verticalJoin = [];
+  for (let arr of newArr) {
+    verticalJoin.push(arr.join(""));
+  }
+
+  // Loop through horizontal array to find if word is included
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  // Loop through horizontal array to find if word is included
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
